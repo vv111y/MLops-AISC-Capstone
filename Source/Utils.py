09e070357df2c96e9cd71fba9ca25ef558d4f0d7
Willy Rempel <willy.rempel@gmail.com>
2019-11-09 16:38:44 -0500

    delete all S3 buckets code snip

--- Source/Utils.py
+++ Source/Utils.py
@@ -1,1 +1,11 @@
 # Miscallaenous useful code goes here
+import boto3
+
+
+# howto delete all your buckets in S3
+s3 = boto3.resource('s3')
+
+for abucket in s3.buckets.all():
+    bucket = s3.Bucket(abucket)
+    bucket.object_versions.delete()
+    bucket.delete()
